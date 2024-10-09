05e1a3ddf9c541134dd62dcbc3e15a2be1e3c3bb
Mathesh M <157198679+Mathesh-299@users.noreply.github.com>
2024-10-09 18:04:37 +0530

    server

--- Server/server.js
+++ Server/server.js
@@ -4,13 +4,13 @@ const app = express();
 const cors = require('cors');
 const DBcon = require('./config/db'); // Ensure DB connection is correctly set up
 const projects = require("./routes/ProjectRoutes");
-const Users = require("./routes/UserRouters");
+// const Users = require("./routes/UserRouters");
 
 app.use(express.json());
 const port = process.env.PORT || 7778;
 app.use(cors());
 app.use('/projects', projects);
-app.use('/users', Users);
+// app.use('/users', Users);
 
 // Uncomment the following code if you want to use the root endpoint
 // app.get('/', (req, res) => {
